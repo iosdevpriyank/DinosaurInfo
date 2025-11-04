027e8a2cdea0d4a5d5ec504e3f508494b099fcc7
Priyank Gandhi <iosdev.priyank@gmail.com>
2025-11-04 23:43:36 +0530

    feat(Initial): Initial application setup
    Initial application setup

---
+++ DinosaurInfo/DinosaurInfo/DinosaurInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinosaurInfoApp.swift
+//  DinosaurInfo
+//
+//  Created by Akshat Gandhi on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DinosaurInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
